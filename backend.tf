f76864513d7e1a23a89138692effc4611eccce67
dinu2907 <82222946+dinu2907@users.noreply.github.com>
2022-07-12 21:29:27 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
+    bucket = "s3-terraform-integration"
     key = "main"
-    region = "us-east-2"
-    dynamodb_table = "my-dynamodb-table"
+    region = "us-east-1"
+    dynamodb_table = "my-dynamoDB_table"
   }
 }
